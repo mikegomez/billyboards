d68076d382812caa258d34241de1e5b881580419
Mike Gomez <mikegomez21@hotmail.com>
2023-12-03 21:51:42 -0600

    update

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors()); // Enable CORS
 
 app.use(express.json());
+
+
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Enable CORS with specific origin
+const corsOptions = {
+  origin: 'https://billboards.eth.limo',
+  methods: 'GET',
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 
 // Define the CORS options
 // const corsOptions = {
@@ -33,15 +46,15 @@ app.use(express.json());
 //     });
 
 
-// Serve static files from the build folder
-app.use(express.static(path.join(__dirname, 'build')));
+
+
 
 // API endpoint to fetch cryptocurrency data
 app.get('/api/cryptocurrency/listings/latest', async (req, res) => {
   try {
     const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
       headers: {
-        'X-CMC_PRO_API_KEY': 'e0fd5e52-7a1f-4efa-82d1-13bd719dccba',
+        'x-cmc_pro_api_key': 'e0fd5e52-7a1f-4efa-82d1-13bd719dccba',
       },
     });
 
